e47ed1f24be957a0b04fc402e98422034af705b9
Bayu <Bayu@DESKTOP-RH7F2PT>
2024-12-30 14:37:14 +0700

    ambil ui

--- src/main/java/Controller/Pertanyaan1Controller.java
+++ src/main/java/Controller/Pertanyaan1Controller.java
@@ -1,22 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
 package Controller;
 
+import DAO.JenisTanahDAO;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 
-/**
- * FXML Controller class
- *
- * @author Bayu
- */
 public class Pertanyaan1Controller implements Initializable {
 
     @FXML
@@ -28,14 +20,24 @@ public class Pertanyaan1Controller implements Initializable {
 
     @FXML
     private ChoiceBox<String> jenisTanahChoicebox;
-    
+    private String[] JenisTanah = {
+        "aluvial",
+        "andosol",
+        "entisol",
+        "grumusol",
+        "humus",
+        "kapur",
+        "latosol",};
 
-    /**
-     * Initializes the controller class.
-     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        jenisTanahChoicebox.getItems().addAll(JenisTanah);
+        jenisTanahChoicebox.setOnAction(this::getKetinggian);
+    }
 
-        // TODO
+    private void getKetinggian(ActionEvent t) {
+        String selectedKetinggian = jenisTanahChoicebox.getValue();
+        System.out.println("Jenis tanah yang dipilih: " + selectedKetinggian);
+        JenisTanahDAO.jenisTanah = jenisTanahChoicebox.getValue();
     }
 }
